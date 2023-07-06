339466c8b9740d693662db54fe6b6f4b47886c37
Maude Clonet <clonet.maude@gmail.com>
2023-07-06 13:10:55 +0200

    fix(draft): try a fix for stomp connection not established
    See also:
    - WAK-33 Draft - fixes

--- front/src/utils/socket.ts
+++ front/src/utils/socket.ts
@@ -2,6 +2,15 @@ import {Stomp} from "@stomp/stompjs";
 
 console.log("Initiating Stomp configuration")
 const client = Stomp.client(import.meta.env.VITE_SOCKET_URL);
+type StompPendingFunction = () => void;
+const pending: StompPendingFunction[] = [];
+
+client.onConnect = () => {
+    console.log("Running " + pending.length + " pending functions ")
+    for (const pendingFunc of pending) {
+        pendingFunc();
+    }
+}
 client.connect({}, () => {
 }, () => {
 })
@@ -9,24 +18,32 @@ client.connect({}, () => {
 const subscriptionPrefix = "/user/topic/";
 const subscriptions = new Set<string>();
 
+function executeOrSchedule(func: () => void) {
+    if (client.connected) {
+        func();
+        return;
+    }
+    pending.push(func);
+}
+
 export function subscribe(path: string, callback: (data: any) => void) {
     if (subscriptions.has(path)) return;
     subscriptions.add(path);
-    client.subscribe(subscriptionPrefix + path, (response) => callback(JSON.parse(response.body)));
+    executeOrSchedule(() => client.subscribe(subscriptionPrefix + path, (response) => callback(JSON.parse(response.body))));
 }
 
 export function subscribeWithoutUserPrefix(path: string, callback: (data: any) => void) {
     if (subscriptions.has(path)) return;
     subscriptions.add(path);
-    client.subscribe(path, (response) => callback(JSON.parse(response.body)));
+    executeOrSchedule(() => client.subscribe(path, (response) => callback(JSON.parse(response.body))));
 }
 
 export function unsubscribe(path: string) {
     if (!subscriptions.has(path)) return;
     subscriptions.delete(path);
-    client.unsubscribe(subscriptionPrefix + path);
+    executeOrSchedule(() => client.unsubscribe(subscriptionPrefix + path));
 }
 
 export function send(path: string, data: any) {
-    client.send("/app/" + path, {}, JSON.stringify(data));
+    executeOrSchedule(() => client.send("/app/" + path, {}, JSON.stringify(data)));
 }
